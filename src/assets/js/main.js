dfab2d4570b06878e2cf46e49a75d0e3a089ed18
Jonny Ortiz <j0nny.0rt1z@yahoo.com>
2022-10-20 14:54:59 -0400

    home page done

---
+++ src/assets/js/main.js
@@ -0,0 +1,34 @@
+'use strict';
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap';
+
+// (function () {
+//    document.addEventListener('DOMContentLoaded', function () {
+//       const navElement = document.querySelector('.nav-container');
+//       const footerElement = document.querySelector('.footer-container');
+
+//       const fetchNav = async () => {
+//          try {
+//             const res = await fetch('../common/nav.html');
+//             const navTemplate = await res.text();
+//             navElement.innerHTML = navTemplate;
+//          } catch (err) {
+//             console.log('Problem loading nav bar', err);
+//          }
+//       };
+
+//       const fetchFooter = async () => {
+//          try {
+//             const res = await fetch('../common/footer.html');
+//             const footerTemplate = await res.text();
+//             footerElement.innerHTML = footerTemplate;
+//          } catch (err) {
+//             console.log('Problem loading footer', err);
+//          }
+//       };
+
+//       fetchNav();
+//       fetchFooter();
+//    });
+// })();
