fa19e2bee11610e954eca4ac8e29c732f75a0519
MrJonnyOrtiz <mr.jonny.ortiz@gmail.com>
2022-11-21 16:28:23 -0500

    made phone numbers clickable

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,12 +1,11 @@
 'use strict';
-
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
 
 // Import footer
-import { myFooter } from './footer';
+import { myFooter } from './customFooter';
 myFooter();
 
 // Import modal
-import { myModal } from './modal';
+import { myModal } from './customModal';
 myModal();
