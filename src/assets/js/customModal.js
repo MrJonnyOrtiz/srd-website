77707b60ca348e15f9c4f38059b3cd27d8e4ab57
MrJonnyOrtiz <mr.jonny.ortiz@gmail.com>
2023-01-03 13:41:19 -0500

    updated ci

--- src/assets/js/customModal.js
+++ src/assets/js/customModal.js
@@ -12,7 +12,7 @@ export const myModal = () => {
             '<div class="modal-header">' +
             '<h1 class="modal-title fs-5" id="modalLabel">Call Now for a FREE QUOTE!</h1>' +
             '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div>' +
-            '<div class="modal-body">Call <a href="tel:9413130263">(941)313-0263</a></div>!' +
+            '<div class="modal-body">Call <a href="tel:9413130263">(941)313-0263</a>!</div>' +
             '<div class="modal-footer">' +
             '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button></div></div></div></div>'
       );
