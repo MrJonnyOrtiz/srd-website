89a9a389cebde0308bbf342664b065efd7652651
MrJonnyOrtiz <mr.jonny.ortiz@gmail.com>
2023-01-05 17:29:47 -0500

    improved styling of modal

--- src/assets/js/customModal.js
+++ src/assets/js/customModal.js
@@ -7,12 +7,12 @@ export const myModal = () => {
       modalEl.insertAdjacentHTML(
          'afterbegin',
          '<div class="modal fade" id="modal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">' +
-            '<div class="modal-dialog">' +
+            '<div class="modal-dialog modal-dialog-centered">' +
             '<div class="modal-content">' +
             '<div class="modal-header">' +
             '<h1 class="modal-title fs-5" id="modalLabel">Call Now for a FREE QUOTE!</h1>' +
             '<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button></div>' +
-            '<div class="modal-body"><a class="nav-link" href="tel:9413130263">Call (941)313-0263</a></div>' +
+            '<div class="modal-body"><a class="nav-link" href="tel:9413130263"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-phone" viewBox="0 0 16 16"><path d="M11 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h6zM5 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H5z"/><path d="M8 14a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/></svg> (941)313-0263</a></div>' +
             '<div class="modal-footer">' +
             '<button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button></div></div></div></div>'
       );
